c2f98800ebca1948278c564e96dee4692e818085
sarthak kanungo <skanungodgacm@gmail.com>
2019-10-06 05:17:41 +0530

    Add commit to DefaultRestController
    Changed System.out to log.info

--- src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
@@ -1,16 +1,27 @@
 package com.course.practicaljava.rest.controller;
 
+import java.time.LocalTime;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class DefaultRestController {
 
 	@GetMapping("/welcome")
 	public String welcome() {
+		log.info(String.join("", "This is ", "Spring BOOT ", "REST API"));
 		return "welcome to Spring Boot";
+	}
+
+	@GetMapping("/time")
+	public String time() {
+		return LocalTime.now().toString();
 	}
 
 }
