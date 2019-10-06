dcef74aecd6a63748d7acd4811c1320a76ca3643
sarthak kanungo <skanungodgacm@gmail.com>
2019-10-06 06:49:52 +0530

    Commit Car.java
    Add getters and setters for new attributes

--- src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/CarRestController.java
@@ -1,23 +1,34 @@
 package com.course.practicaljava.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.course.practicaljava.rest.domain.Car;
 import com.course.practicaljava.rest.service.CarService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/car/v1")
+@Slf4j
 public class CarRestController {
 
 	@Autowired
 	private CarService carService;
 
-	@GetMapping(path = "/random", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/random")
 	public Car random() {
+		return carService.generateCar();
+	}
+
+	@PostMapping(path = "/echo")
+	public Car echo(@RequestBody Car car) {
+
+		log.info("This car is :-" + car);
 		return carService.generateCar();
 	}
 
--- src/main/java/com/course/practicaljava/rest/domain/Car.java
+++ src/main/java/com/course/practicaljava/rest/domain/Car.java
@@ -1,14 +1,21 @@
 package com.course.practicaljava.rest.domain;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Car {
 
-	String brand;
-	String color;
-	String type;
+	private String brand;
+	private String color;
+	private String type;
+	private int price;
+	private boolean available;
+	private Date firstReleaseDate;
 
 }
